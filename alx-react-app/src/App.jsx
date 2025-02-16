cb97215f6e6bab115fcf23cef35855941f607c28
Zelalem Zerihun <zelalem.zerihun.b@gmail.com>
2025-02-16 18:36:25 +0300

    task 3 done

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import UserProfile from './components/UserProfile'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
