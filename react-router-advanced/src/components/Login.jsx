d66a96166a647787cfb62f38ff17fa48a59be665
Zelalem Zerihun <zelalem.zerihun.b@gmail.com>
2025-03-09 13:49:11 +0300

    task-2.8

--- react-router-advanced/src/components/Login.jsx
+++ react-router-advanced/src/components/Login.jsx
@@ -1,8 +1,14 @@
-const Login = ({ setAuth }) => (
-  <div>
-    <h2>Login Page</h2>
-    <button onClick={() => setAuth(true)}>Log In</button>
-  </div>
-);
+import useAuth from "./useAuth";
+
+const Login = () => {
+  const { login } = useAuth(); // Use the login function from useAuth
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <button onClick={login}>Log In</button>
+    </div>
+  );
+};
 
 export default Login;
--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,6 +1,9 @@
+import { useAuth } from "./useAuth"; // Import the useAuth hook
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ isAuthenticated, children }) => {
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth(); // Use the authentication state
+
   return isAuthenticated ? children : <Navigate to="/login" replace />;
 };
 
