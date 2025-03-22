a0b630502c83484d3f3336b349e27562640c4006
Zelalem Zerihun <zelalem.zerihun.b@gmail.com>
2025-03-22 23:36:02 +0300

    updated

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-
-export default {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
-  theme: { extend: {} },
-  variants: { extend: {} },
-  plugins: [],
-};
+ 
+ export default {
+   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+   darkMode: false, // You can set it to 'media' or 'class' if needed
+   theme: {
+     extend: {},
+   },
+   plugins: [],
+ };
