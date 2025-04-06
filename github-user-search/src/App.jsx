b66893e950a867fb2b9c1762ec0852b41f57e008
Zelalem Zerihun <zelalem.zerihun.b@gmail.com>
2025-04-06 23:23:26 +0300

    task 0

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import.meta.env.VITE_APP_GITHUB_API_KEY;
 function App() {
   const [count, setCount] = useState(0)
 
